6bf75f0d0db9557af57b8bff2a4c78babfef1404
Nikita Timofeev <stariy95@gmail.com>
2017-06-01 18:08:18 +0300

    Add some java code and SQL scripts

--- README.md
+++ README.md
@@ -7,23 +7,24 @@ You will need [JDK](http://www.oracle.com/technetwork/java/javase/downloads/jdk8
  
 ## Usage
 - Clone this repo: `git clone https://github.com/stariy95/cayenne-maven-demo.git`
+
+- Create demo DB using on of `sql` scripts provided in `src/main/resources` directory
+
 - Setup DB connection in `pom.xml`:
 ```xml
 <dataSource>
-    <url>jdbc:mysql://localhost/db_name</url>
+    <url>jdbc:mysql://localhost/demo</url>
     <driver>com.mysql.jdbc.Driver</driver>
     <username>root</username>
     <password>password</password>
 </dataSource>
 ```
-- Setup filters in `pom.xml`:
-```xml
-<dbImport>
-    <!-- cdbimport settings -->
-</dbImport>
-```
+and in `src/main/resources/cayenne-project.xml` (edit DataNode via Modeler)
+
 - Run `mvn cayenne:cdbimport` to create data map from DB
 - Run `mvn cayenne:cgen` to generate classes 
+- Run `mvn package` to compile Demo and create Jar file
+- Run this demo `java -jar target/cayenne-maven-demo-1.0-SNAPSHOT.jar`
 
 ## Usefull links
 - [Including Cayenne in Project](http://cayenne.apache.org/docs/4.0/cayenne-guide/including-cayenne-in-project.html#maven-projects)
---
+++ src/main/java/org/apache/cayenne/demo/Main.java
@@ -0,0 +1,91 @@
+package org.apache.cayenne.demo;
+
+import java.time.LocalDate;
+import java.util.List;
+
+import org.apache.cayenne.ObjectContext;
+import org.apache.cayenne.configuration.server.ServerRuntime;
+import org.apache.cayenne.demo.model.Artist;
+import org.apache.cayenne.demo.model.Gallery;
+import org.apache.cayenne.demo.model.Painting;
+import org.apache.cayenne.query.ObjectSelect;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        // starting Cayenne
+        ServerRuntime cayenneRuntime = ServerRuntime.builder()
+                .addConfig("cayenne-project.xml")
+                .build();
+
+        // getting a hold of ObjectContext
+        ObjectContext context = cayenneRuntime.newContext();
+
+        newObjectsTutorial(context);
+        selectTutorial(context);
+        deleteTutorial(context);
+    }
+
+    static void newObjectsTutorial(ObjectContext context) {
+
+        // creating new Artist
+        Artist picasso = context.newObject(Artist.class);
+        picasso.setName("Pablo Picasso");
+        picasso.setDateOfBirth(LocalDate.of(1881, 10, 25));
+
+        // Creating other objects
+        Gallery metropolitan = context.newObject(Gallery.class);
+        metropolitan.setName("Metropolitan Museum of Art");
+
+        Painting girl = context.newObject(Painting.class);
+        girl.setName("Girl Reading at a Table");
+
+        Painting stein = context.newObject(Painting.class);
+        stein.setName("Gertrude Stein");
+
+        // connecting objects together via relationships
+        picasso.addToPaintings(girl);
+        picasso.addToPaintings(stein);
+
+        girl.setGallery(metropolitan);
+        stein.setGallery(metropolitan);
+
+        // saving all the changes above
+        context.commitChanges();
+    }
+
+    static void selectTutorial(ObjectContext context) {
+        // SelectQuery examples
+        List<Painting> paintings1 = ObjectSelect
+                .query(Painting.class)
+                .select(context);
+
+        List<Painting> paintings2 = ObjectSelect
+                .query(Painting.class, Painting.NAME.likeIgnoreCase("gi%"))
+                .select(context);
+
+        List<Painting> paintings3 = ObjectSelect
+                .query(Painting.class)
+                .where(Painting.ARTIST.dot(Artist.DATE_OF_BIRTH).lt(LocalDate.of(1900, 1, 1)))
+                .select(context);
+
+    }
+
+    static void deleteTutorial(ObjectContext context) {
+
+        Artist picasso = ObjectSelect
+                .query(Artist.class, Artist.NAME.eq("Pablo Picasso"))
+                .selectOne(context);
+
+        if (picasso != null) {
+            List<Painting> picassoPaintings = ObjectSelect
+                    .query(Painting.class, Painting.ARTIST.eq(picasso))
+                    .select(context);
+            context.deleteObjects(picassoPaintings);
+            context.deleteObjects(picasso);
+            context.commitChanges();
+        }
+    }
+
+}
